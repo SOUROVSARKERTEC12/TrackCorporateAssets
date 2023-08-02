7227e4ac3f6a1d9ab8ee8e2d5afbf4c6103fad71
Admin_laravel <sourovsarker005@gmail.com>
2023-08-03 04:42:19 +0600

    show list of companies

--- TrackCorporateAssets/urls.py
+++ TrackCorporateAssets/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('assets.urls')),
     path('admin/', admin.site.urls),
 ]
